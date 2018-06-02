0683b85a43c7ef4bc5db4470948b00154b901627
balatkvl <gbalatkvl@gmail.com>
2018-06-02 15:38:35 +0530

    fc

---
+++ fib.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class fibo
+{
+public static void main(String arg[])throws IOException
+{
+int a=-1,b=1,c=0,num=0;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("enter the n");
+num=Integer.parseInt(br.readLine());
+for(int i=0;i<=num;i++)
+{
+c=a+b;
+a=b;
+b=c;
+System.out.println(c);
+}
+}
+}
+
